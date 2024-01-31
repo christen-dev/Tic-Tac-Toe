67739d8060c0e124858d17a6c5be8e287b45fc65
christensen-dev <dogsippepsi@gmail.com>
2024-01-31 10:34:54 -0700

    Created "TicTacTools" Class with methods "PrintBoard()" and "GetResults"

---
+++ TicTacTools.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+    internal class TicTacTools
+    {
+        public void PrintBoard(char[] boardArray)
+        {
+            for (int i = 0; i < 3; i++) //Print the first row with the values from the array
+            {
+                Console.WriteLine(boardArray[i] + " | ");
+            }
+
+            Console.WriteLine("---------");
+
+            for (int i = 3; i < 6; i++)
+            {
+                Console.WriteLine(boardArray[i] + " | "); //Second Row
+            }
+
+            Console.WriteLine("---------");
+
+            for (int i = 6; i < 9; i++)
+            {
+                Console.WriteLine(boardArray[i] + " | "); //Third Row
+            }
+
+        }
+
+
+        public string GetResults(char[] boardArray)
+        {
+            if ()
+        }
+
+    }
+}
